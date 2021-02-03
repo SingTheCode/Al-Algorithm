d6f2abd11619a7b4de769b5134c63acce49a6bfd
spiderq2697 <spiderq10@gmail.com>
2021-02-03 22:13:12 +0900

    Sorting01.py 작성

---
+++ Sorting/Sorting01.py
@@ -0,0 +1,23 @@
+def solution(array, commands):
+    tmp = []
+    answer = []
+    i = 0
+    a = 0
+    b = 0
+
+    while i < 3:
+        a = commands[i][0] - 1
+        b = commands[i][1]
+        c = commands[i][2] - 1
+
+        # b가 7일 경우 리스트의 길이보다 커서 인식이 안되는 상황 방지
+        if b < 7:
+            tmp = array[a:b]
+        else:
+            tmp = array[a:]
+
+        tmp.sort()
+        answer.append(tmp[c])
+        i = i + 1
+
+    return answer
