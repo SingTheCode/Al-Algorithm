aab24dc1e115e77c0308283fcc2f5857bf90e3a1
spiderq10 <65025333+spiderq2697@users.noreply.github.com>
2021-02-04 22:26:53 +0900

    Update Sorting01.py

--- Sorting/Sorting01.py
+++ Sorting/Sorting01.py
@@ -2,19 +2,14 @@ def solution(array, commands):
     tmp = []
     answer = []
     i = 0
-    a = 0
-    b = 0
 
     while i < len(commands):
         a = commands[i][0] - 1
         b = commands[i][1]
         c = commands[i][2] - 1
 
-        if b < len(array):
-            tmp = array[a:b]
-        else:
-            tmp = array[a:]
-
+        tmp = array[a:b]
+        
         tmp.sort()
         answer.append(tmp[c])
         i = i + 1
