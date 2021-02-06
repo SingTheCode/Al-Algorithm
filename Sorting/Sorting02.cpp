b3ac3d548283208eac7f69d40ee9c8bb5eb0c7f9
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-06 19:09:01 +0900

    Sorting02

---
+++ Sorting/Sorting02.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include<iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+bool compare(int, int);
+string solution(vector<int> numbers) {
+    string answer = "";
+    int count=0;
+    
+    sort(numbers.begin(),numbers.end(),compare);
+
+    for(int i=0,j=numbers.size();i<j;i++){
+        if(numbers[i]==0)
+            count++;
+        answer+=to_string(numbers[i]);
+    }
+    if(count==numbers.size())
+        answer="0";
+
+    return answer;
+}
+bool compare(int previousNumber,int nextNumber){
+    string previousToNext=to_string(previousNumber)+to_string(nextNumber);
+    string nextToPrevious=to_string(nextNumber)+to_string(previousNumber);
+    int previousToNextNumber=stoi(previousToNext);
+    int nextToPreviousNumber=stoi(nextToPrevious);
+
+    return previousToNextNumber>nextToPreviousNumber;
+}
