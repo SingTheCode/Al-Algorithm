9b64f78d26a8db056e7c6bd84ca644f89b2cb8e8
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-13 18:08:38 +0900

    Hash01_unorderd_set사용

--- Hash/Hash01.cpp
+++ Hash/Hash01.cpp
@@ -1,12 +1,12 @@
 #include <string>
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
-    multiset<string> st(participant.begin(),participant.end());
+    unordered_multiset<string> st(participant.begin(),participant.end());
     for(int i=0,j=completion.size();i<j;i++){
         auto it=st.find(completion[i]);
         st.erase(it);
