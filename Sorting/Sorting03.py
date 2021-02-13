785d6daeeda6ded8b836cb68dfe63acc6b96d714
spiderq2697 <spiderq10@gmail.com>
2021-02-13 22:19:29 +0900

    Sorting03.py 작성 but 정답률 87.5%

---
+++ Sorting/Sorting03.py
@@ -0,0 +1,22 @@
+def solution(citations):
+    i = 0
+    j = 0
+    hMax = 0
+    moreThanHCount = 0
+
+    while (i < len(citations)):
+        while (j < len(citations)):
+            if (hMax <= citations[j]):
+                moreThanHCount = moreThanHCount + 1
+            j = j + 1
+        if (hMax <= moreThanHCount):
+            hMax = hMax + 1
+            j = 0
+            i = i + 1
+            moreThanHCount = 0
+        else:
+            return hMax - 1
+    return hMax
+
+arr = [9,9,9,9,9]
+print(solution(arr))
