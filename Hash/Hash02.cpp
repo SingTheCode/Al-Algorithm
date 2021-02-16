01041f5e17f089bfb1a1fa08f20b949b8c6e6761
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-16 22:09:08 +0900

    Update Hash02.cpp

--- Hash/Hash02.cpp
+++ Hash/Hash02.cpp
@@ -9,8 +9,8 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     string left,right;
     multiset<string> st(phone_book.begin(),phone_book.end());
-    for(auto it=st.begin();it!=st.end();it++){
-        for(auto it2=st.begin();it2!=st.end();it2++){           // iterator는 초기값을 begin()+1과 같이 설정할 수 없어 begin()부터 설정
+    for(auto it=st.begin()+;it!=st.end();it++){
+        for(auto it2=it;it2!=st.end();it2++){           
             if(it!=it2){                                        // 같은 위치의 값을 방지하여 비교
                 left=*it;
                 right=*it2;
