6d277f1e7095fdf0269d560477e1c571b706dcb1
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-18 22:27:27 +0900

    Update Hash02.cpp

--- Hash/Hash02.cpp
+++ Hash/Hash02.cpp
@@ -9,7 +9,7 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     string left,right;
     multiset<string> st(phone_book.begin(),phone_book.end());
-    for(auto it=st.begin()+;it!=st.end();it++){
+    for(auto it=st.begin();it!=st.end();it++){
         for(auto it2=it;it2!=st.end();it2++){           
             if(it!=it2){                                        // 같은 위치의 값을 방지하여 비교
                 left=*it;
