e29977a6b1dc78a7d6d0038f30f3873d3af6258a
sunghyuki <sunghyuk1609@gmail.com>
2021-02-20 16:30:11 +0900

    modify: 전화번호 목록 로직 수정 및 README 추가

--- Hash/Hash01_1.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    i = 0
-
-    while(i < len(completion)):
-        check = completion[i] in participant
-        if(check == True):
-            participant.remove(completion[i])
-            i = i + 1
-    return participant[0]
