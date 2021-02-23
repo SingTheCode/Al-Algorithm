84ab71ba831dae8492f1a881ccc70eff38a1acd9
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-02-23 17:01:22 +0900

    Stack_Queue01(description)

--- Stack_Queue/Stack_Queue01.cpp
+++ Stack_Queue/Stack_Queue01.cpp
@@ -5,27 +5,26 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int answer = 0,sum = 0;
-    queue <int>q;
+    int answer = 0,sum = 0;                             // sum은 무게의 총합
+    queue<int> bridge;
+    
     do{
-        if(q.size()==bridge_length){
-            sum-=q.front();
-            q.pop();
+        if(bridge.size()==bridge_length){               // bridge의 끝에 트럭이 도착했을경우
+            sum-=bridge.front();
+            bridge.pop();
         }
-        if(!truck_weights.empty()){
-            int target=truck_weights.front();
-            if(weight<(sum+target)){
-                q.push(0);
-            }else{
-                q.push(target);
-                sum+=target;
-                truck_weights.erase(truck_weights.begin());
-            }
-        }else{
-            q.push(-1);
+        int target=truck_weights.front();               // 다음 트럭의 무게
+        if(weight<(sum+target)){                        // 다음트럭과 무게의 총합이 무게를 초과할 경우
+            bridge.push(0);
+        }else{                                          // 초과하지 않을경우
+            bridge.push(target);                        
+            sum+=target;
+            truck_weights.erase(truck_weights.begin());
         }
         answer++;
-    }while(q.front()!=-1);
-    
+    }while(!truck_weights.empty());
+
+    answer+=bridge_length;                              // 마지막 트럭이 통과하는 시간을 더해준다.
+
     return answer;
 }
